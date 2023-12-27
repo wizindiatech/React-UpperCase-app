312017d6673b5c514f777fee8bc78ccb3348c268
Chinmaya Pradhan <cp.smaket@gmail.com>
2023-12-27 12:40:02 +0530

    commit 2 name change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import TextForm from './components/TextForm';
 function App() {
   return (
     <>
-      <Navbar title="Profile" myHome="Menu" aboutText="About" />
+      <Navbar title="Profile" myHome="Home" aboutText="About" />
       <div className="container my-3">
         <TextForm heading= "UPPERCASE CONVERTER"/>
       </div>
